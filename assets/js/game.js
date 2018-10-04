bbde4f6a4bc5ef1c9710e06ca2ddad7da2165130
Kyle Scheuerlein <kscheuerlein@icloud.com>
2018-10-04 14:52:47 -0700

    animate svg
    - save day/night preference in cookie

--- assets/js/game.js
+++ assets/js/game.js
@@ -45,6 +45,7 @@ var hangman = {
 		}
 		if( wrongGuess == hangman.answer.length ) {
 			hangman.guesses--;
+			svgAnimator.draw(hangman.guesses);
 			//hangman.placeGuessesRemaining();
 		}
 		if( hangman.answer == hangman.guessedWordStr() && hangman.guesses > 0) {
@@ -54,12 +55,14 @@ var hangman = {
 		}
 	},
 	win: function() {
-		alert("You win!");
+		document.getElementById("win-lose").innerHTML = "You win!";
+		document.getElementById("win-lose").style.visibility = "visible";
 		document.getElementById("guess-input").children[0].disabled = true;
 		document.getElementById("new-game").children[0].style.visibility = "visible";
 	},
 	lose: function() {
-		alert("You lose!");
+		document.getElementById("win-lose").innerHTML = "You lose!";
+		document.getElementById("win-lose").style.visibility = "visible";
 		document.getElementById("guess-input").children[0].disabled = true;
 		document.getElementById("new-game").children[0].style.visibility = "visible";
 		for(var t = 0; t < hangman.answer.length; t++ ) {
@@ -73,6 +76,11 @@ var hangman = {
 		}
 	},
 	init: function() {
+		var s = get_cookie("style");
+		if( s != undefined ) {
+			selectStyle(s);	
+		}
+		document.getElementById("win-lose").style.visibility = "hidden";
 		hangman.chooseWord();
 		hangman.guesses = 10;
 		//hangman.placeGuessesRemaining();
@@ -81,9 +89,76 @@ var hangman = {
 		hangman.guessedWord = [];
 		document.getElementById("new-game").children[0].style.visibility = "hidden";
 		document.getElementById("guess-input").children[0].disabled = false;
-		selectStyle("day");
+		svgAnimator.draw(hangman.guesses);
 	}
 };
+
+var svgAnimator = {
+	draw: function(x) {
+		if( x == 10 ) {
+			for( var i = 0; i < 10; i++ ) {
+				var path = document.querySelector('#svg_' + i);
+				var length = path.getTotalLength();
+				// Clear any previous transition
+				path.style.transition = path.style.WebkitTransition =
+				  'none';
+				// Set up the starting positions
+				path.style.strokeDasharray = length + ' ' + length;
+				path.style.strokeDashoffset = '0';
+				// Trigger a layout so styles are calculated & the browser
+				// picks up the starting position before animating
+				path.getBoundingClientRect();
+				// Define our transition
+				path.style.transition = path.style.WebkitTransition =
+				  'stroke-dashoffset 2s ease-in-out';
+				// Go!
+				path.style.strokeDashoffset = length;
+			}
+		} else {
+			var path = document.querySelector('#svg_' + x);
+			var length = path.getTotalLength();
+			// Clear any previous transition
+			path.style.transition = path.style.WebkitTransition =
+			  'none';
+			// Set up the starting positions
+			path.style.strokeDasharray = length + ' ' + length;
+			path.style.strokeDashoffset = length;
+			// Trigger a layout so styles are calculated & the browser
+			// picks up the starting position before animating
+			path.getBoundingClientRect();
+			// Define our transition
+			path.style.transition = path.style.WebkitTransition =
+			  'stroke-dashoffset 2s ease-in-out';
+			// Go!
+			path.style.strokeDashoffset = '0';
+		}
+	}
+};
+
+function save_cookies(key, value) {
+	var exdays = 120; // cookies will expire in 120 days
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires=" + d.toGMTString();
+	// save cookies with expiration date
+	document.cookie = key + "=" + value + ";" + expires;
+}
+
+function get_cookie(key) {
+	var name = key + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 function isLetter(str) {
 	return str.length === 1 && str.match(/[a-z]/i);
@@ -91,11 +166,6 @@ function isLetter(str) {
 
 function processInput(ltr) {
 	var l = ltr[0]; // in case of fast typing, grab only first char
-	var abc = ["a", "b", "c", "d", "e", "f",
-		  "g", "h", "i", "j", "k", "l",
-		  "m", "n", "o", "p", "q", "r",
-		  "s", "t", "u", "v", "w", "x",
-		  "y", "z", ];
 	if(l != undefined) { // fast typing results in undefined
 		if( isLetter(l) ) {
 			l = l.toLowerCase(); // convert to lower case letter
@@ -106,6 +176,7 @@ function processInput(ltr) {
 }
 
 function selectStyle(s) {
+	save_cookies("style", s);
 	document.getElementById("day-night-stylesheet").setAttribute("href", "assets/css/" + s + ".css");
 }
 
